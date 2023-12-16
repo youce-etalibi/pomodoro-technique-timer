d67313bac5568367149d667ed08f9d88e89dae0f
Youssef Talibi <youssef.talibi11@gmail.com>
2023-12-16 13:58:18 +0100

    v2 silver mode

--- src/components/z - homepage.jsx
+++ src/components/z - homepage.jsx
@@ -24,7 +24,7 @@ export default function HomePage() {
     <Fragment>
       <div className={Mode === "silver" ? "silver" : "naturale"}>
         <div className="Application">
-          <BrowserRouter>
+          <BrowserRouter basename="/pomodoro-tech-timer">
           {/* <h1>{Mode }</h1> */}
             <Routes>
               <Route
